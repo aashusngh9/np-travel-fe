62c166edcec420cef3fc6746bc063682a19fa6e7
Ashutosh Singh <ashutosh.singh@blueoptima.com>
2024-10-13 18:56:43 +0530

    Working version 1.1.1

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,10 @@ RUN npm install
 
 COPY . .
 
-RUN npm run build  # Build the React app
+ARG REACT_APP_API_BASE_URL
+ENV REACT_APP_API_BASE_URL=$REACT_APP_API_BASE_URL
+
+RUN npm run build
 
 FROM nginx:alpine
 
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the image name
+image_name="airfare-fe"
+
+# Build the Docker image
+#docker build --platform="linux/amd64" -t $image_name .
+docker build --platform="linux/amd64" --build-arg REACT_APP_API_BASE_URL='https://ec2-13-51-234-10.eu-north-1.compute.amazonaws.com' -t $image_name .
+
+# Save the Docker image as a tar file
+docker save $image_name > $image_name.tar
+
+# Upload the tar file to the server using scp
+#scp -i /Users/singhas/p-projects/first-key-pair.pem $image_name.tar ubuntu@ec15-51-20-94-47.eu-north-1.compute.amazonaws.com:~
+scp -i "/Users/singhas/p-projects/first-key-pair.pem" $image_name.tar ubuntu@ec2-13-51-234-10.eu-north-1.compute.amazonaws.com:~
+
+# Clean up the tar file (optional)
+#rm $image_name.tar
